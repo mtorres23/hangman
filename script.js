098e2c6a1fb3249be34052acfc05a4b9a351ea98
michael <michael.atorres90@gmail.com>
2017-03-09 14:18:43 -0500

    added js

--- script.js
+++ script.js
@@ -1,6 +1,80 @@
-window.onload = function () {
+var wordBank = ["sould food", "greek", "japanese", "spanish", "american", "indian", "cajun", "cajun", "thai", "caribbean", "italian"];
+	function wordBank () {
+		rand = Math.floor(Math.random()*wordBank.length);
+		word = wordBank[rand];
 
-  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-        't', 'u', 'v', 'w', 'x', 'y', 'z'];
-        
+	document.getElementById()
+	}
+
+
+function draw() {
+var canvas = document.getElementById("hangman");
+var ctx = canvas.getContext('2d');
+
+var grd=ctx.createLinearGradient(0,0,170,0);
+grd.addColorStop(0,"black");
+grd.addColorStop(1,"gray");
+grd.addColorStop(0.5,"white");
+
+        ctx.fillStyle = grd;
+        ctx.lineWidth=2;
+        ctx.fillRect(0, 0, 300, 300);
+        ctx.beginPath(); //vertical bar
+            ctx.moveTo(50,295);
+            ctx.lineTo(50,25);
+            ctx.stroke();
+        ctx.beginPath(); //vertical bar long piece
+            ctx.moveTo(65,295);
+            ctx.lineTo(65,85);
+            ctx.stroke();
+        ctx.beginPath(); //vertical bar short piece
+            ctx.moveTo(65,64);
+            ctx.lineTo(65,40);
+            ctx.stroke();
+        ctx.beginPath(); //horizontal bar
+            ctx.moveTo(49,25);
+            ctx.lineTo(175,25);
+            ctx.stroke();
+        ctx.beginPath(); //horizontal bar short piece
+            ctx.moveTo(49,40);
+            ctx.lineTo(86,40);
+            ctx.stroke();
+        ctx.beginPath(); //horizontal bar long piece
+            ctx.moveTo(106,40);
+            ctx.lineTo(175,40);
+            ctx.stroke();
+        ctx.beginPath(); //small vertical bar
+            ctx.moveTo(173,25);
+            ctx.lineTo(173,40);
+            ctx.stroke();
+        ctx.beginPath(); //cross bar
+            ctx.moveTo(50,80);
+            ctx.lineTo(100,25);
+            ctx.stroke();
+        ctx.beginPath(); //cross bar
+            ctx.moveTo(60,90);
+            ctx.lineTo(111,35);
+            ctx.stroke();
+        ctx.beginPath(); //cross bar
+            ctx.moveTo(50,80);
+            ctx.lineTo(60,90);
+            ctx.stroke();
+        ctx.beginPath(); //cross bar
+            ctx.moveTo(100,25);
+            ctx.lineTo(111,35);
+            ctx.stroke();
+        ctx.beginPath(); //ground
+            ctx.moveTo(35,295);
+            ctx.lineTo(265,295);
+            ctx.stroke();
+        ctx.beginPath(); //noose
+            ctx.moveTo(150,40);
+            ctx.lineTo(150,80);
+            ctx.stroke();
+}
+
+
+
+
+
+
